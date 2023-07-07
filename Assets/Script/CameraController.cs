4311281e513d7ae0dbe03c6aba016c841bc26355
Yang <45927105+yang2158@users.noreply.github.com>
2023-07-07 12:36:12 -0700

    Camera stuff

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
+    public float cameraSpeed = 1;
+    public float zoomSpeed = 30;
+    public float TargetPos = 0;
+    public float TargetZoom = 0;
+    public float lerpSmooth = 0.1f;
+    public float lerpSmoothZ = 0.02f;
+    Camera cam = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GetComponent<Camera>();
+        TargetZoom = transform.position.z;
+        TargetPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xAxis = Input.GetAxis("Horizontal");
+        float zoom = Input.GetAxis("Mouse ScrollWheel") + Input.GetAxis("Vertical")*0.05f; 
+        TargetZoom += zoom*zoomSpeed;
+        TargetPos += xAxis* cameraSpeed;
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Lerp(transform.position.x, TargetPos, lerpSmooth); 
+        pos.z = Mathf.Lerp(transform.position.z, TargetZoom, lerpSmoothZ);
+        transform.position = pos; 
     }
 }
